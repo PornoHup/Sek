ad687ba05aa496e63eb63c1441851f03ee855bd3
Elnur <104248864+GenceliElnur@users.noreply.github.com>
2022-05-30 00:39:04 +0400

    update botTesla.js

--- botTesla.js
+++ botTesla.js
@@ -168,7 +168,7 @@ const RaundMesajHusnuEhedov = (chatId, round, time) => {
 
 	return Degisken(`
 		*🔹 Raund ${round + 1}/${process.env.RAUND_SAYI}*
-		❓ Sizcə Bu Kişinin Neçə Yaşı Var
+		🧔🏻 Yaşını Təxmin Et!
 		${answers.length > 0 ? 
 			`\n${answers.map((member, index) => `${index + 1}. *${member.firstName}*: ${member.answer}`).join("\n")}\n`
 			:
