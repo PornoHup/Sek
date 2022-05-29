bd38b7978cd1f2e9750c0963484b530570188942
Elnur <104248864+GenceliElnur@users.noreply.github.com>
2022-05-29 20:31:20 +0400

    update botTesla.js

--- botTesla.js
+++ botTesla.js
@@ -394,8 +394,8 @@ ${(top).sort((a, b) => b.score - a.score).slice(0, 20).map((member, index) => `$
 
 bot.command("help", (ctx) => {
     return ctx.replyWithMarkdown(Degisken(`
-        *Salam! "Tahimin" oyunu üçün\n oluşturulmuş bir botam🤖*\n🆘*Bot sadəcə gruplar üçün hazırlanmışdır!*\n\n_Qaydalar👇🏻_ : Mən sizə bir şəkil göndərdiyim zaman kateqoriyaya uyğun rəqəmlərlə təxmin edəcəksiniz, bu qədər asandır.🕵🏼‍♂ ,❕ Əvvəlcə məni bir qrupa əlavə edin və sonra /tesla əmrini işə salın. 🎯(Qrupun media icazəsi açıq olmasını unutmayın.)🗣\n_Sonra Komutlarla ile oyunu başladın_🎯\n
-          *Əmrlər siyahı👇🏻*\n\n🎲 /elegant - _Oyunu Başlat_\n⛔️ /stop - _Oyunu Dayandır_\n📊 /top - _Qrub Oyunçuların Xalları_\n_🌍 /gtop - _Global TOP 20 Oyunçular_\nℹ️ /help - _Bot Haqqında_\n👤 /kullanici - _Qrup ID sini Göstərər_\n🆔 /id - _Grup infosu_`))
+        *Salam! "Tahimin" oyunu üçün\n  oluşturulmuş bir botam🤖*\n 🆘*Bot sadəcə gruplar üçün hazırlanmışdır!*\n\n _Qaydalar👇🏻_ : Mən sizə bir şəkil göndərdiyim zaman kateqoriyaya uyğun rəqəmlərlə təxmin edəcəksiniz, bu qədər asandır.🕵🏼‍♂ ,❕ Əvvəlcə məni bir qrupa əlavə edin və sonra /elegant əmrini işə salın. 🎯(Qrupun media icazəsi açıq olmasını unutmayın.)🗣\n _Sonra Komutlarla ile oyunu başladın_🎯\n
+          *Əmrlər siyahı👇🏻*\n\n 🎲 /elegant - _Oyunu Başlat_\n ⛔️ /stop - _Oyunu Dayandır_\n 📊 /top - _Qrub Oyunçuların Xalları_ \n 🌍 /gtop - _Global TOP 20 Oyunçular_\n ℹ️ /help - _Bot Haqqında_\n 👤 /kullanici - _Qrup ID sini Göstərər_\n 🆔 /id - _Grup infosu_`))
 })
 
 bot.command("kullanici", async (ctx) => {
