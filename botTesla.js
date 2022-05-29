d84d7759d17fd7c8e94b4119dacb8a90edc0762f
Elnur <104248864+GenceliElnur@users.noreply.github.com>
2022-05-29 22:44:45 +0400

    update botTesla.js

--- botTesla.js
+++ botTesla.js
@@ -395,7 +395,7 @@ ${(top).sort((a, b) => b.score - a.score).slice(0, 20).map((member, index) => `$
 bot.command("help", (ctx) => {
     return ctx.replyWithMarkdown(Degisken(`
         *Salam! "Təxmin" oyunu üçün\n  oluşturulmuş bir botam🤖*\n \n 🆘*Bot sadəcə gruplar üçün hazırlanmışdır!*\n \n _Qaydalar👇🏻_ \n \n Mən sizə bir şəkil göndərdiyim zaman kateqoriyaya uyğun rəqəmlərlə təxmin edəcəksiniz, bu qədər asandır.🕵🏼‍♂ , \n \n ❕ Əvvəlcə məni bir qrupa əlavə edin və sonra /elegant əmrini işə salın. \n \n 🎯(Qrupun media icazəsi açıq olmasını unutmayın.)🗣\n \n _Sonra Komutlarla ile oyunu başladın_🎯\n
-          *Əmrlər siyahı👇🏻*\n\n 🎲 /elegant - _Oyunu Başlat_\n ⛔️ /stop - _Oyunu Dayandır_\n 📊 /top - _Qrub Oyunçuların Xalları_ \n 🌍 /gtop - _Global TOP 20 Oyunçular_\n ℹ️ /help - _Bot Haqqında_\n 👤 /kullanici - _Qrup ID sini Göstərər_\n 🆔 /id - _Grup infosu_`))
+          *Əmrlər siyahı👇🏻*\n\n 🎲 /elegant - _Oyunu Başlat_\n ⛔️ /stop - _Oyunu Dayandır_\n 📊 /top - _Qrub Oyunçuların Xalları_ \n 🌍 /gtop - _Global TOP 20 Oyunçular_\n ℹ️ /help - _Bot Haqqında_\n 👤 /kullanici - _Öz Profilinə Bax_\n 🆔 /id - _Grup infosu_`))
 })
 
 bot.command("kullanici", async (ctx) => {
